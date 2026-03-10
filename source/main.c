07e29c347c64938c65dd00a5911df030e727ad29
Varvara.Lanina <laninavarvara.mail.ru>
2026-03-10 10:39:49 +0300

    main.c+

--- source/main.c
+++ source/main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <stdint.h>
+
+#define CHIP8_MEMORY_SIZE	4096
+#define CHIP8_DISPLAY_WIDTH	64
+#define CHIP8_DISPLAY_HEIGHT	32
+#define V_REGISTER_COUNT	16
+#define STACK_DEPTH		16
+#define CHIP8_KEY_COUNT		16
 
 int main() {
+	typedef struct Chip8State{
+		int a;
+		int b;
+	};
 	printf("Hello, World!\n");
 
 	return 0;
